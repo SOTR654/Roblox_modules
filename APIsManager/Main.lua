052fc2116f890b201b91b9c02d238978f1aa294f
SOTR654 <r.sotr654@gmail.com>
2022-10-02 17:32:21 -0400

    Update Main.lua

--- APIsManager/Main.lua
+++ APIsManager/Main.lua
@@ -1,1 +1,17 @@
+local Multiple = require(script.Multiple)
 
+
+
+--			Constructor			--
+local Constructor = {}
+function Constructor.new(ScriptId: string)
+	local GoogleUrl = "https://script.google.com/macros/s/" .. ScriptId .. "/exec"
+	
+	local self = {}
+	self.UsersAPI = require(script.APIs.UsersAPI)(GoogleUrl)
+	self.AvatarAPI = require(script.APIs.AvatarAPI)(GoogleUrl)
+	self.GroupsAPI = require(script.APIs.GroupsAPI)(GoogleUrl)
+	self.CatalogAPI = require(script.APIs.CatalogAPI)(GoogleUrl)
+	return self
+end
+return Constructor
